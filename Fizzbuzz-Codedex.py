8f4d4e0ef1155208b1559b4733619d157dd2897d
rrz.chy <rrz.chy@gmail.com>
2025-08-31 18:47:06 -0400

    Update Fizzbuzz-Codedex.py

--- Fizzbuzz-Codedex.py
+++ Fizzbuzz-Codedex.py
@@ -3,13 +3,14 @@ n = int(input("Num"))
 
 def fizzbuzz(n):
     for i in range(n):
-        if i % 3 == 0:
+        if i % 15 == 0:
             print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
-        elif i % 15 == 0:
+        elif i % 3 == 0:
             print("Fizzbuzz")
         else:
             print(i)
+
 
 fizzbuzz(n)
